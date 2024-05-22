5fa796877368a773dab4eae53a3244dd5bbda4c5
Mohammed Swafvan <swafvansafu07@gmail.com>
2024-05-22 16:27:28 +0530

    starter of the flutter quiz application

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/presentation/screens/home/home_page.dart';
+import 'package:get/route_manager.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Quiz App',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const HomePage(),
+    );
+  }
+}
